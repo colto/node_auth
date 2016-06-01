8fe081b1aa0c465e8a9f2c9c767e3fd1cb78cfd2
colto <coltomatic@gmail.com>
2016-06-01 09:46:11 -0700

    some proper message

--- router.js
+++ router.js
@@ -7,7 +7,7 @@ const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   app.get('/', requireAuth, function(req,res) {
-    res.send({ hi: 'there' });
+    res.send({ message: 'You are authorized to see this stuff, ya dig?' });
   })
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
